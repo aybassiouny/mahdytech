2652af4e5afc8ed357d0e7cb7b0c6e2ffd69e680
aybassiouny <aybassiouny@gmail.com>
2020-04-26 12:49:30 -0700

    Add hiddne input

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -45,7 +45,8 @@ class BlogPostTemplate extends React.Component {
         />
         <Bio />
 
-        <form name="contact" method="POST" data-netlify="true">
+        <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>Your Name: <input type="text" name="name" /></label>   
           </p>
